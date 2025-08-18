82b04cb6b88ce2156623f80fd8ea3d419d68f063
zacksmash <zachary.warren@me.com>
2025-08-18 11:28:17 -0600

    WIP 🚧

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import { wayfinder } from "@laravel/vite-plugin-wayfinder";
 
 export default defineConfig({
     plugins: [
