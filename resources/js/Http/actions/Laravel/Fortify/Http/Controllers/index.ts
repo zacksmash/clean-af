db4dfb18f86d2610e21bfa243787a676b6e0e332
zacksmash <zachary.warren@me.com>
2025-08-19 15:39:09 -0600

    WIP 🚧

--- resources/js/Http/actions/Laravel/Fortify/Http/Controllers/index.ts
+++ resources/js/Http/actions/Laravel/Fortify/Http/Controllers/index.ts
@@ -6,6 +6,12 @@ import ProfileInformationController from './ProfileInformationController'
 import PasswordController from './PasswordController'
 import ConfirmablePasswordController from './ConfirmablePasswordController'
 import ConfirmedPasswordStatusController from './ConfirmedPasswordStatusController'
+import TwoFactorAuthenticatedSessionController from './TwoFactorAuthenticatedSessionController'
+import TwoFactorAuthenticationController from './TwoFactorAuthenticationController'
+import ConfirmedTwoFactorAuthenticationController from './ConfirmedTwoFactorAuthenticationController'
+import TwoFactorQrCodeController from './TwoFactorQrCodeController'
+import TwoFactorSecretKeyController from './TwoFactorSecretKeyController'
+import RecoveryCodeController from './RecoveryCodeController'
 
 const Controllers = {
     AuthenticatedSessionController,
@@ -16,6 +22,12 @@ const Controllers = {
     PasswordController,
     ConfirmablePasswordController,
     ConfirmedPasswordStatusController,
+    TwoFactorAuthenticatedSessionController,
+    TwoFactorAuthenticationController,
+    ConfirmedTwoFactorAuthenticationController,
+    TwoFactorQrCodeController,
+    TwoFactorSecretKeyController,
+    RecoveryCodeController,
 }
 
 export default Controllers
