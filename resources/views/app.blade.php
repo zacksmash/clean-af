9aa606c2915f21979d9ec9469d2a242e0eef156d
zacksmash <zachary.warren@me.com>
2025-08-21 15:39:01 -0600

    WIP 🚧

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.ts'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
