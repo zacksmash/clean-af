201ee476f28fc9d96cd39a726ecdd36b9b5ef460
zacksmash <zachary.warren@me.com>
2025-09-10 15:08:26 -0600

    WIP 🚧

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,12 +1,12 @@
-import { createInertiaApp } from '@inertiajs/vue3';
-import createServer from '@inertiajs/vue3/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, DefineComponent, h } from 'vue';
-import { renderToString } from 'vue/server-renderer';
+import { createInertiaApp } from '@inertiajs/vue3'
+import createServer from '@inertiajs/vue3/server'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { DefineComponent, createSSRApp, h } from 'vue'
+import { renderToString } from 'vue/server-renderer'
 
 import AppLayout from '@/layouts/AppLayout.vue'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createServer(
     (page) =>
@@ -24,4 +24,4 @@ createServer(
             setup: ({ App, props, plugin }) => createSSRApp({ render: () => h(App, props) }).use(plugin),
         }),
     { cluster: true },
-);
+)
